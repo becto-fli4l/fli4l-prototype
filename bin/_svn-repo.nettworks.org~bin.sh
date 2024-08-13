ba5e99d183e1bd29435be831bfae848e547f3881
Tobias Becker <fli4l@becker.link>
2024-08-13 11:40:33 +0200

    add becto-fli4l binary download script

---
+++ bin/_svn-repo.nettworks.org~bin.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# _svn-repo.nettworks.org~bin.sh: download binary files from svn (r60808)
+# -> https://nettworks.atlassian.net/wiki/spaces/f/pages/3377491/Verzeichnisstruktur+eines+fli4l+SVN+checkouts
+
+svn checkout -r60808 https://repo.nettworks.org/svn/fli4l/branches/4.0/trunk/bin/x86_64
+
+# ---//
